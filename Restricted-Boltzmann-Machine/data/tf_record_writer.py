a0fb7ade6865a9d8d947bac2ad735c29d0088954
BaomingLee <44024527+BaomingLee@users.noreply.github.com>
2019-08-04 23:29:00 +0100

    8.4

--- Restricted-Boltzmann-Machine/data/tf_record_writer.py
+++ Restricted-Boltzmann-Machine/data/tf_record_writer.py
@@ -50,7 +50,7 @@ def run(output_dir):
 
     SAMPLES_PER_FILES=100
     
-    training_set, test_set=_get_dataset('100k')
+    training_set, test_set=_get_dataset('1M')
 
     for data_set, name, dir_ in zip([training_set, test_set], ['train', 'test'], output_dir):
         
