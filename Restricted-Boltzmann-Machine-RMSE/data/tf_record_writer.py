ce061f728879fbcf79d1f5026b6d204505fad155
BaomingLee <44024527+BaomingLee@users.noreply.github.com>
2019-08-05 11:18:40 +0100

    Update tf_record_writer.py

--- Restricted-Boltzmann-Machine-RMSE/data/tf_record_writer.py
+++ Restricted-Boltzmann-Machine-RMSE/data/tf_record_writer.py
@@ -4,6 +4,7 @@ import os
 import sys
 from preprocess_data import _get_dataset
 
+
 OUTPUT_DIR_TRAIN = os.path.abspath(os.path.join(os.path.dirname(__file__), '.' + '/tf_records_1M/train'))
 OUTPUT_DIR_TEST = os.path.abspath(os.path.join(os.path.dirname(__file__), '.' + '/tf_records_1M/test'))
 
